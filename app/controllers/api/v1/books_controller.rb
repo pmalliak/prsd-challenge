79b436fd6eed68a610dfefc7267ef55dd7dcc85c
Panos Malliakoudis <panos.malliakoudis@gmail.com>
2021-03-17 23:15:00 +0200

    Add CRUD methods for books

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::BooksController < ApplicationController
+  before_action :set_book, only: [:show, :update, :destroy]
+  
+  def index
+    @books = Book.visible.published.includes(:author).order("authors.last_name")
+  end
+  
+  def show
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if !@book.save
+      render :json => {:message => @book.errors.full_messages}, status: 500
+    end
+  end
+  
+  def update
+    if !@book.update(book_params)
+      render :json => {:message => "Error"}, status: 500
+    end
+  end
+  
+  def destroy
+    if @book.destroy
+      render :json => {:message => "Success"}, status: 200
+    else
+      render :json => {:message => "Error"}, status: 500
+    end
+  end
+  
+  private
+  def set_book
+    @book =  Book.find_by_id(params[:id])
+    if @book.blank?
+      render :json => {:message => "Book not found"}, status: 404
+    end
+  end
+  
+  def book_params
+    params.permit(:title, :description, :isbn, :creation_date, :visible, :author_id, :publisher_id)
+  end
+end
