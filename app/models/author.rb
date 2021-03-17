5284fa22f3cbded55695839733bddadb99f94104
Panos Malliakoudis <panos.malliakoudis@gmail.com>
2021-03-17 23:13:57 +0200

    Add model relations and helper methods

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ApplicationRecord
+  has_many :books
+  
+  def full_name
+    return [self.first_name, self.last_name].join(" ")
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  belongs_to :author
+  belongs_to :publisher, optional: true
+  
+  scope :visible,    -> {where(:visible => true)}
+  scope :published,  -> {joins(:publisher)}
 end
